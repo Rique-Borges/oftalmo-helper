be9c81544a3c02a34603b73e42c2e4425c17eae8
Rique-Borges <henriqueribeiroborges@gmail.com>
2026-08-03 09:54:41 -0300

    change: mudança de escala

--- src/app/conversor-ponto/page.tsx
+++ src/app/conversor-ponto/page.tsx
@@ -87,7 +87,7 @@ export default function AfdConverter() {
     3: "08:00", // Quarta
     4: "08:00", // Quinta
     5: "08:00", // Sexta
-    6: "00:00", // Sábado
+    6: "04:00", // Sábado
     0: "00:00"  // Domingo
   });
 
